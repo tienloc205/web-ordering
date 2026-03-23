1de0e303b87abc6337e7546aa0e9a7a3851dd794
tienloc205 <160756312+tienloc205@users.noreply.github.com>
2026-03-23 16:58:39 +0700

    fix gitignore

--- backend/src/main/java/com/example/backend/controller/Test.java
+++ backend/src/main/java/com/example/backend/controller/Test.java
@@ -1,4 +1,15 @@
 package com.example.backend.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class Test {
+    @GetMapping("/test")
+    public String apiTest() {
+        return "Hello World";
+    }
 }
